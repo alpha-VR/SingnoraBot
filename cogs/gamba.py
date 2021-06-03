a3d158400e07eb169333a9cced5fb12a7e1a8da2
IgnisSpirtus <63767562+IgnisSpirtus@users.noreply.github.com>
2021-06-03 21:38:31 +0530

    final fifiy fifty bot (#1)

--- cogs/gamba.py
+++ cogs/gamba.py
@@ -3,14 +3,20 @@ from discord.ext import commands
 import random
 import sys
 import os
-
+import random
+from random import randint
 class Gamba(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
-    
+    #ctx.send
     @commands.command()
-    async def ping(self, ctx):
-        await ctx.send(f'Bot ping is: {round(self.bot.latency,3)*1000} ms')
+    async def fifty(self, ctx):
+        x=randint(0,1)
+        if x==1:
+            await ctx.send(f"Yes,{ctx.message.author.mention} will win the fifty/fifty for the featured character!! <:hopium:849339347534741584>")
+        else:
+            await ctx.send(f'Im sorry,{ctx.message.author.mention} will lose the fifty/fifty for the featured character <:pepe_copium:847462168319885332>')
     
 def setup(bot):
     bot.add_cog(Gamba(bot))
+
