0dc51b4786d62d2e34c2025a0b5823d53522d2e0
IgnisSpirtus <63767562+IgnisSpirtus@users.noreply.github.com>
2021-06-04 10:37:36 +0530

    Roast Me command added  (#3)
    * final fifiy fifty bot
    
    * signora kick
    
    * roastme command added
    
    Co-authored-by: KVR-ADITYA <34878205+KVR-ADITYA@users.noreply.github.com>

--- cogs/basic.py
+++ cogs/basic.py
@@ -3,7 +3,8 @@ from discord.ext import commands
 import random
 import sys
 import os
-
+from random import randint
+import json
 class Basic(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
@@ -15,8 +16,17 @@ class Basic(commands.Cog):
     @commands.command()
     async def signora(self,ctx):
         await ctx.send('Signora will come home',file = discord.File('./data/images/signora.png'))
+    
     @commands.command()
-    async def kickme(self,ctx):
+    async def kickme(self,ctx):   
         await ctx.send(f'Signora kicked {ctx.message.author.mention}',file=discord.File('./data/gif/kick.gif'))
+    
+    @commands.command()
+    async def roastme(self,ctx):
+        f = open('./data/roasts.json')
+        data = json.load(f)
+        i=randint(0,len(data)-1)
+        await ctx.send(data[i]['submission'])
+
 def setup(bot):
     bot.add_cog(Basic(bot))
